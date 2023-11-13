3c29e9923218278296f409f698239e704498881a
desikaaa <desikarmila211@gmail.com>
2023-11-14 00:19:47 +0700

    modifikasi

--- LinearSearch07.java
+++ LinearSearch07.java
@@ -1,16 +1,31 @@
+ import java.util.Scanner;
 public class LinearSearch07 {
 
     public static void main(String[] args) {
-        int [] arrayInt = {34, 18, 26, 48, 72, 20, 56, 63};
-        int key = 20;
+        Scanner sc = new Scanner (System.in);
+        int [] arrayInt = new int [6];
+        int key = 0;
         int hasil = 0;
 
+        System.out.print("Masukkan jumlah elemen array :");
+        int jmlElemen = sc.nextInt(); 
         for (int i = 0; i < arrayInt.length; i++){
+            System.out.print("Masukkan elemen array ke-"+i+" :");
+            arrayInt[i] = sc.nextInt();
+        }
+        System.out.print("Masukkan array yang ingin dicari :");
+         key = sc.nextInt();
+        for (int i = 0; i < arrayInt.length; i++) {
             if (arrayInt[i] == key){
                 hasil = i;
                 break;
             }
-        }
-        System.out.println("Key ada dalam array pada posisi indeks ke-"+hasil);
+            }
+            if (hasil != 0) {
+            System.out.println("Key ada di posisi indeks ke-"+hasil);
+            }else {
+            System.out.println("Key tidak ditemukan");
+            }  
     }
 }
+
